39d0db186ac3a493c29d567805a646e4eef929b7
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-13 10:48:43 +0100

    ADD fill alphabet upper and lower hash AND MOD upper/lower letter method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,8 +1,8 @@
 # Program that will cipher a String that the user will pass to it.
 
 class Caesar
-    ALPHABET_LOWERCASE = Array ('a'..'z')
     ALPHABET_UPPERCASE = Array ('A'..'Z')
+    ALPHABET_LOWERCASE = Array ('a'..'z')
 
     def initialize(text, num)
         @text = text
@@ -21,13 +21,33 @@ class Caesar
 
     attr_reader :text, :num
     
+    # Will create a hash for the uppercase letter or the lower case ones
+    def fill_alphabet_upper
+        return @fill_alphabet_upper if defined? @fill_alphabet_upper
+        @fill_alphabet_upper = Hash.new
+        ALPHABET_UPPERCASE.each_with_index do |key, l|
+            @fill_alphabet_upper[key.to_sym] = l
+        end
+        @fill_alphabet_upper
+    end
+
+    def fill_alphabet_lower
+        return @fill_alphabet_lower if defined? @fill_alphabet_lower
+        @fill_alphabet_lower = Hash.new
+        ALPHABET_LOWERCASE.each_with_index do |key, l|
+            @fill_alphabet_lower[key.to_sym] = l
+        end
+        @fill_alphabet_lower
+    end
+
     def refactor_text(text)
         new_text = ""
         text.each do |letter|
+            
             # Checks if the character is withing the alphabet list, if not, it will just write it in the new word
-            if ALPHABET_LOWERCASE.include?(letter) || ALPHABET_UPPERCASE.include?(letter)
+            if fill_alphabet_lower.key?(letter.to_sym) || fill_alphabet_upper.key?(letter.to_sym)
                 # Checks if the letter given is lowercase or uppercase
-                new_text += ALPHABET_LOWERCASE.include?(letter).nil? ? uppper_letter(letter) : lower_letter(letter)
+                new_text += fill_alphabet_upper.key?(letter.to_sym) ? uppper_letter(letter.to_sym) : lower_letter(letter.to_sym)
                 next
             end
             new_text += letter
@@ -37,27 +57,41 @@ class Caesar
 
     # Private method that transforms lowercase letters
     def lower_letter(letter)
-        new_letter_index = ALPHABET_LOWERCASE.find_index(letter).to_i+num
-        # Checks if the index number is within the length of the alphabet, if it's not it will start form the start of the alphabet
-        if new_letter_index > ALPHABET_LOWERCASE.length-1
-            new_letter_index = new_letter_index-ALPHABET_LOWERCASE.length
+        new_letter_value = fill_alphabet_lower[letter].to_i
+        num_change = num < 0? -1 : 1
+        for v in 1..num.abs do
+            new_letter_value += num_change
+            # Checks if the index number is within the length of the alphabet, if it's not it will start form the start of the alphabet or from the end
+            if new_letter_value > fill_alphabet_lower.length()-1
+                new_letter_value = 0
+            end
+            if new_letter_value < 0
+                new_letter_value = fill_alphabet_lower.length()-1
+            end
         end
-        ALPHABET_LOWERCASE[new_letter_index]
+        return fill_alphabet_lower.key(new_letter_value).to_s
     end
 
     # Private method that transforms uppercase letters
     def uppper_letter(letter)
-        new_letter_index = ALPHABET_UPPERCASE.find_index(letter).to_i+num
-        # Checks if the index number is within the length of the alphabet, if it's not it will start form the start of the alphabet
-        if new_letter_index > ALPHABET_UPPERCASE.length-1
-            new_letter_index = new_letter_index-ALPHABET_UPPERCASE.length
+        new_letter_value = fill_alphabet_upper[letter].to_i
+        num_change = num < 0? -1 : 1
+        for v in 1..num.abs do
+            new_letter_value += num_change
+            # Checks if the index number is within the length of the alphabet, if it's not it will start form the start of the alphabet or from the end
+            if new_letter_value > fill_alphabet_upper.length()-1
+                new_letter_value = 0
+            end
+            if new_letter_value < 0
+                new_letter_value = fill_alphabet_upper.length()-1
+            end
         end
-        ALPHABET_UPPERCASE[new_letter_index]
+        return fill_alphabet_upper.key(new_letter_value).to_s
     end
 end 
 
 # Code tests
 text_positive_number = Caesar.new("Hey There", 3).cipher
-text_negative_number = Caesar.new("Hey There", -3).cipher
+text_negative_number = Caesar.new("Heb There", -300).cipher
 text_not_valid_text = Caesar.new("", 1).cipher
 text_not_valid_number = Caesar.new("Hey There", 0).cipher
