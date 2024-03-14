9af3f4ed595c0a75ba60f9370827ab159e409088
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-14 14:00:58 +0100

    MOD Class name to Runner

--- runnable.rb
+++ runnable.rb
@@ -1,21 +1,20 @@
 require_relative 'caesar_cipher'
 # Class that will ask the user for input for the cipher method of the Caesar class
 # It will repite this process until the user says something other tha y(Yes)
-class Runnable
+class Runner
   def run
-    continue = true
-    while continue
+    while true
       puts 'Write a text to cipher'
       text = gets.chomp
       puts 'Input a number to cifer the text'
       num = gets.chomp.to_i
       puts Caesar.new(text, num).cipher
       puts 'Continue ciphering?(Y/N)'
-      continue = false unless gets.chomp.upcase == 'Y'
+      break unless gets.chomp.upcase == 'Y'
     end
   rescue Caesar::TextEmptyError, Caesar::NumberZeroError => e
     puts e.message
   end
 end
 
-Runnable.new.run
+Runner.new.run
