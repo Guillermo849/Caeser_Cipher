84fb0918bf6252cc687a3d7a33853a67070ba004
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-14 13:48:34 +0100

    MOD class so it manages the execution of Caesar class

--- runnable.rb
+++ runnable.rb
@@ -1,18 +1,21 @@
 require_relative 'caesar_cipher'
-# Child class from Caesar class that will ask input from the user
-class Runnable < Caesar
-  # It will check if there is text to cipher and if there is a valid number to change the letters
-  def initialize
-    puts 'Write a text to cipher'
-    text = gets.chomp
-    raise StandardError, 'The text given is empty.' if text.strip.empty?
-
-    puts 'Input a number to cifer the text'
-    num = gets.chomp.to_i
-    raise StandardError, 'The number given cannot be 0.' if num.zero?
-
-    super(text, num)
+# Class that will ask the user for input for the cipher method of the Caesar class
+# It will repite this process until the user says something other tha y(Yes)
+class Runnable
+  def run
+    continue = true
+    while continue
+      puts 'Write a text to cipher'
+      text = gets.chomp
+      puts 'Input a number to cifer the text'
+      num = gets.chomp.to_i
+      puts Caesar.new(text, num).cipher
+      puts 'Continue ciphering?(Y/N)'
+      continue = false unless gets.chomp.upcase == 'Y'
+    end
+  rescue Caesar::TextEmptyError, Caesar::NumberZeroError => e
+    puts e.message
   end
 end
 
-puts Runnable.new.cipher
+Runnable.new.run
