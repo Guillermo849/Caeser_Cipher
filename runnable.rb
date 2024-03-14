d2e187dca663b4cf504cb3a624f49347dbbc355a
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-14 14:20:43 +0100

    MOD Forever loop to ask questions until the user doesn't say Y(Yes)

--- runnable.rb
+++ runnable.rb
@@ -3,17 +3,19 @@ require_relative 'caesar_cipher'
 # It will repite this process until the user says something other tha y(Yes)
 class Runner
   def run
-    while true
-      puts 'Write a text to cipher'
-      text = gets.chomp
-      puts 'Input a number to cifer the text'
-      num = gets.chomp.to_i
-      puts Caesar.new(text, num).cipher
-      puts 'Continue ciphering?(Y/N)'
-      break unless gets.chomp.upcase == 'Y'
+    loop do
+      begin
+        puts 'Write a text to cipher'
+        text = gets.chomp
+        puts 'Input a number to cifer the text'
+        num = gets.chomp.to_i
+        puts Caesar.new(text, num).cipher
+        puts 'Continue ciphering?(Y/N)'
+        break unless gets.chomp.upcase == 'Y'
+      rescue Caesar::TextEmptyError, Caesar::NumberZeroError => e
+        puts e.message
+      end
     end
-  rescue Caesar::TextEmptyError, Caesar::NumberZeroError => e
-    puts e.message
   end
 end
 
