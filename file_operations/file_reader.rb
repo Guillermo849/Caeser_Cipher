6a7e673d4bdcd5890d33aeda65a37016876b302a
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-20 10:14:52 +0100

    MOD Set class reader and writer under module FileOperations

---
+++ file_operations/file_reader.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module FileOperations
+  # It will read the files.
+  class FileReader
+    Error = Class.new(StandardError)
+    NameEmptyError = Class.new(Error)
+    FileNotFoundError = Class.new(Error)
+
+    def initialize(name:)
+      @name = name
+    end
+
+    def read_file
+      raise NameEmpty, 'File name empty' if name.chomp.empty?
+      raise FileNotFound, 'File not found' if File.file?(name.chomp)
+
+      File.foreach("#{name}.txt") { |line| puts line }
+    end
+
+    private
+
+    attr_reader :name
+  end
+end
