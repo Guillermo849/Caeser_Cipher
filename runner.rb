87026588944265c46b2f3eb8806b199f2fa73870
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-20 10:16:08 +0100

    Include the FileOperations module

--- runner.rb
+++ runner.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 require_relative 'caesar_cipher'
-require_relative 'caesar_files'
+require_relative 'file_operations/file_reader'
+require_relative 'file_operations/file_writer'
+
 # Class that will ask the user for input for the cipher method of the Caesar class
 # It will repite this process until the user says something other tha y(Yes)
 class Runner
+  include FileOperations
   def run
     loop do
+      # Read previous files
+      puts 'Would you like to read previous files?(Y/N)'
+      if gets.chomp.upcase == 'Y'
+        puts Dir['*.txt']
+        puts 'What file would you like to read?'
+        file_reader = FileOperations::FileReader.new(name: gets.chomp)
+        file_reader.read_file
+      end
       # Cipher text
       puts 'Write a text to cipher'
       text = gets.chomp
@@ -18,20 +29,12 @@ class Runner
       puts 'Write the name of the file'
       file_name = gets.chomp
       text = "#{cipher_text} - #{text}"
-      file_writer = FileWriter.new(name: file_name, text: text)
+      file_writer = FileOperations::FileWriter.new(name: file_name, text: text)
       file_writer.write_file
-      # Read previous files
-      puts 'Would you like to read previous files?(Y/N)'
-      if gets.chomp.upcase == 'Y'
-        puts Dir['*.txt']
-        puts 'What file would you like to read?'
-        file_reader = FileReader.new(name: gets.chomp)
-        file_reader.read_file
-      end
       # Question to restart the loop
       puts 'Would you like to cipher a new code?(Y/N)'
       break unless gets.chomp.upcase == 'Y'
-    rescue Caesar::IpnutErrors, FileReader::ReadErrros, FileWrighter::WriterErros => e
+    rescue Caesar::Error, FileOperations::FileReader::Error, FileOperations::FileWriter::Error => e
       puts e.message
     end
   end
